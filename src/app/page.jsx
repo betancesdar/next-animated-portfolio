2dc956120cb8efc924401c2842ada227953f55a7
Dario Betances <betancesdar08@gmail.com>
2025-01-04 00:09:29 -0400

    Feature: pagfe,navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return <div className='h-full flex flex-col'>
+    {/* IMAGE CONTAINER */}
+    <div className='h-1/2 relative bg-blue-200'>
+      <Image src="/hero.png" alt="hero" fill/>
+    </div>
+    {/* TEXT CONTAINER */}
+    <div className='h-1/2'>
+
+    </div>
+  </div>;
 };
 
 export default Homepage;
